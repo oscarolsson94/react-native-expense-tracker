a7bc610813fe151bdbd0120227208d5b9cbaf2eb
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-11-01 09:02:13 +0100

    add trashcan icon

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,20 +1,31 @@
 import { useLayoutEffect } from "react";
 import { View, Text } from "react-native";
+import { IconButton } from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
 export const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
 
   const isEditing = !!editedExpenseId;
 
+  const deleteExpenseHandler = () => {};
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: isEditing ? "Edit Expense" : "Add Expense",
     });
-  });
+  }, [isEditing, navigation]);
 
   return (
     <View>
-      <Text>ManageExpense</Text>
+      {isEditing && (
+        <IconButton
+          icon="trash"
+          color={GlobalStyles.colors.error500}
+          size={36}
+          onPress={deleteExpenseHandler}
+        />
+      )}
     </View>
   );
 };
