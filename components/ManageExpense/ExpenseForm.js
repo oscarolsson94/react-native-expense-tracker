d728554c8ececc1836911633efa7653be7d969d7
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-11-01 13:30:25 +0100

    added options to inputs

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -2,11 +2,35 @@ import { View, Text } from "react-native";
 import { Input } from "./Input";
 
 export const ExpenseForm = () => {
+  const amountChangedHandler = () => {};
+
+  /*   const handleChange = (event) => {
+    setContactInfo({ ...contactInfo, [event.target.name]: event.target.value });
+  }; */
+
   return (
     <View>
-      <Input label="Amount" />
-      <Input label="Date" />
-      <Input label="Description" />
+      <Input
+        label="Amount"
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: amountChangedHandler,
+        }}
+      />
+      <Input
+        label="Date"
+        textInputConfig={{
+          placeholder: "YYYY-MM-DD",
+          maxLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+        }}
+      />
     </View>
   );
 };
