367252e155553aee2f86fcc64f4f8589b29ab15e
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-11-01 09:05:06 +0100

    styling

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,5 @@
 import { useLayoutEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { IconButton } from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 
@@ -17,15 +17,32 @@ export const ManageExpense = ({ route, navigation }) => {
   }, [isEditing, navigation]);
 
   return (
-    <View>
+    <View style={styles.container}>
       {isEditing && (
-        <IconButton
-          icon="trash"
-          color={GlobalStyles.colors.error500}
-          size={36}
-          onPress={deleteExpenseHandler}
-        />
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
