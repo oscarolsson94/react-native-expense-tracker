d57264391129c203f5aadb0432ded7c9f6656833
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-11-03 15:09:16 +0100

    adding loading spinner

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,10 +1,13 @@
 import { useContext, useEffect } from "react";
 import { ExpensesOutput } from "../components/ExpensesOutput";
+import { LoadingOverlay } from "../components/UI/LoadingOverlay";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../utils/date";
 import { fetchExpenses } from "../utils/http";
 
 export const RecentExpenses = () => {
+  const [isFetching, setIsFetching] = useState(true);
+
   const { expenses, setExpenses } = useContext(ExpensesContext);
 
   const recentExpenses = expenses.filter((expense) => {
@@ -16,12 +19,16 @@ export const RecentExpenses = () => {
 
   useEffect(() => {
     const getExpenses = async () => {
+      setIsFetching(true);
       const expenses = await fetchExpenses();
+      setIsFetching(false);
       setExpenses(expenses);
     };
 
     getExpenses();
   }, []);
+
+  if (isFetching) return <LoadingOverlay />;
 
   return (
     <ExpensesOutput
